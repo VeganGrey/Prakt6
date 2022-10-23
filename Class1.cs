c421af64bdb72165bc94321cc9a4865b2747f7bc
doctorflex <doctorflex@DESKTOP-RQVSRNU>
2022-10-23 12:43:52 +0300

    Добавьте файлы проекта.

---
+++ Class1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prakt6
+{
+    internal class Pair
+    {
+        /// <summary>
+        /// Класс для пар чисел и операций сравнения и вычитания
+        /// </summary>
+        public int Pair1 { get; set; }
+        public int Pair2 { get; set; }
+        /// <summary>
+        /// Метод для Проверки чисел первой пары на равенство
+        /// </summary>
+        /// <returns>Логическое значение "верно" или "неверно"</returns>
+        /// 
+        public Pair()
+        {
+            Pair1 = 0;
+            Pair2 = 0;
+        }
+
+        public bool Prov()
+        {
+            return Pair1 == Pair2;
+        }
+        /// <summary>
+        /// Метод для нахождения произведния чисел в первой паре
+        /// </summary>
+        /// <returns>Произведение первого числа пары на второе</returns>
+        public int Peremnozh()
+        {
+            return Pair1 * Pair2;
+        }
+        /// <summary>
+        /// Метод для нахождения разности первой пары и второй пары
+        /// </summary>
+        /// <param name="value">Объект класса обозначающий вторую пару</param>
+        /// <param name="dop1">Значение от разности первого числа перв пары и второй пары</param>
+        /// <param name="dop2">Значение от разности второго числа перв пары и второй пары</param>
+        public void Vichit(Pair value, out int dop1, out int dop2)
+        {
+            dop1 = Pair1 - value.Pair1;
+            dop2 = Pair2 - value.Pair2;
+        }
+        /// <summary>
+        /// Метод для нахождения разности первой,второй и третьей пары
+        /// </summary>
+        /// <param name="value1">Объект обозначающий вторую пару</param>
+        /// <param name="value2">Объект обозначающий третью пару</param>
+        /// <param name="dop1">Значение разности первых чисел перв пары,второй и третьей</param>
+        /// <param name="dop2">Значение разности вторых чисел перв пары,второй и третьей</param>
+        public void Vichit(Pair value1, Pair value2, out int dop1, out int dop2)
+        {
+            dop1 = Pair1 - value1.Pair1 - value2.Pair1;
+            dop2 = Pair2 - value1.Pair2 - value2.Pair2;
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Prakt6
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Pair para = new Pair();
+        Pair para2 = new Pair();
+        Pair para3 = new Pair();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+      
+        private void Proverka(object sender, RoutedEventArgs e)
+        {
+            Int32.TryParse(pair1.Text, out int p1); Int32.TryParse(pair2.Text, out int p2);
+            para.Pair1 = p1; para.Pair2 = p2;
+            if (para.Prov()) Itog.Text = "Числа равны";
+            else Itog.Text = "Числа не равны";
+        }
+
+        private void Proizv(object sender, RoutedEventArgs e) //Проба
+        {
+            Int32.TryParse(pair1.Text, out int p1); Int32.TryParse(pair2.Text, out int p2);
+            para.Pair1 = p1; para.Pair2 = p2;
+            Itog.Text = Convert.ToString(para.Peremnozh());
+        }
+
+        private void Otric(object sender, RoutedEventArgs e)
+        {
+            Int32.TryParse(pair1.Text, out int p1); Int32.TryParse(pair2.Text, out int p2);
+            para.Pair1 = p1; para.Pair2 = p2;
+            Int32.TryParse(pair3.Text, out int p3); Int32.TryParse(pair4.Text, out int p4);
+            para2.Pair1 = p3; para2.Pair2 = p4;
+            para.Vichit(para2, out int paraNov1, out int paraNov2);
+            Itog.Text = Convert.ToString($"{paraNov1};{paraNov2}");
+        }
+
+        private void TriPari(object sender, RoutedEventArgs e)
+        {
+            Int32.TryParse(pair1.Text, out int p1); Int32.TryParse(pair2.Text, out int p2);
+            para.Pair1 = p1; para.Pair2 = p2;
+            Int32.TryParse(pair3.Text, out int p3); Int32.TryParse(pair4.Text, out int p4);
+            para2.Pair1 = p3; para2.Pair2 = p4;
+            Int32.TryParse(pair5.Text, out int p5); Int32.TryParse(pair6.Text, out int p6);
+            para3.Pair1 = p5; para3.Pair2 = p6;
+            para.Vichit(para2, para3, out int paraNov1, out int paraNov2);
+            Itog.Text = Convert.ToString($"{paraNov1};{paraNov2}");
+        }
+
+        private void Spravka(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Калитин Сергей ИСП-31, Вариант 13\n Задание:\n Создать базовый класс Pair (пара целых чисел)." +
+                "Создать необходимые методы и свойства.Определить методы с операциями проверки на равенство и перемножения полей." +
+                " Реализовать операцию вычитания пар по формуле (а, b) - (с, d) = (а - c, b - d).Создать перегруженный метод для вычитания трех пар чисел.");
+        }
+
+        private void Quit(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Clear(object sender, RoutedEventArgs e)
+        {
+            Itog.Clear();
+            pair1.Clear();
+            pair2.Clear();
+            pair3.Clear();
+            pair4.Clear();
+            pair5.Clear();
+            pair6.Clear();
+        }
+    }
+}
+
